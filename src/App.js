414f58deb97fa7b97569ba02097ce0265f70654a
ajaygbl <ajaygbl@gmail.com>
2024-03-24 15:36:40 +0530

    Added count of num of advices read till now

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import { useState } from "react";
 
 function App() {
 
-const [advice, setadvice] = useState("");
+const [advice, setAdvice] = useState("");
+const [count, setCount] = useState(0);
 
 async function getAdvice(){
 const res = await fetch("https://api.adviceslip.com/advice");
 const data = await res.json();
-setadvice(data.slip.advice);
+setAdvice(data.slip.advice);
+setCount((c) => c+1);
 }
 
 return (
 <div className="App">
 <h1>{advice}</h1>
 <button onClick={getAdvice}>Get Advice</button>
+<p>you have read {count} advices till now</p>
 </div>
 );
 }
